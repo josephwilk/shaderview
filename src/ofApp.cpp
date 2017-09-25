5a7af7a540b04919bd0961036b09eae2af2897b5
Joseph Wilk <joe@josephwilk.net>
2017-09-25 10:55:16 +0100

    faster

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -52,11 +52,8 @@ void ofApp::setup(){
   currentAmp = 1.0;
 
   defaultVert = STRINGIFY(
-                          uniform mat4 modelViewProjectionMatrix;
-
                           void main(){
-                            vec4 pos = gl_ProjectionMatrix * gl_ModelViewMatrix * gl_Vertex;
-                            gl_Position = pos;
+                            gl_Position = ftransform();
                             gl_TexCoord[0] = gl_MultiTexCoord0;
                             gl_FrontColor = gl_Color;
                           }
